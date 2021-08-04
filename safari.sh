eaa2f2d7ac7f756f12e9b282e87d3c534002ca7a
all-tool-vip <88178692+all-tool-vip@users.noreply.github.com>
2021-08-04 09:26:44 +0700

    Add files via upload

---
+++ safari.sh
@@ -0,0 +1,44 @@
+clear
+#kode warna
+MERAH="\033[31;1m"
+HIJAU="\033[32;1m"
+echo -e $MERAH
+figlet "safari..."
+
+echo -e $HIJAU
+
+echo "[+]------------------------[+]"
+echo "[+] AUTHOR : EPAN & Friend [+]"
+echo "[+] Thanks to : R4F-13X    [+]"
+echo "[+] KONTAK :+6285871424841 [+]"
+echo "------------MENU--------------"
+echo "[1] Crash Safari all droid"
+echo "------------------------------"
+		                 
+read -p "di pilih slur : " pil
+echo "Pilihan anda $pil "
+
+echo "Starting safari..."
+sleep 3
+echo "Crash HP :   bit.ly/2V3yjwu"
+
+echo -e $MERAH
+
+echo "!!! Warning : jika handphone mati Tanggung sendiri"
+
+sleep 3
+
+echo -e $HIJAU
+echo "---------------------------"
+echo "BERMAIN HACKING FAKE"
+echo "---------------------------"
+
+read -p "Nomor target : " target
+echo "Sedang Mengirim virus ke $target"
+
+echo -e $MERAH
+echo "Mengirim fake virus ke $target"
+sleep 1
+echo "Mengirim fake virus ke $target"
+sleep 1
+echo "Mengirim fake virus ke $target"
